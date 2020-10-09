60b9c8cb87531b9b7a6161c1a3a712c784730fe3
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-10-09 08:53:50 -0700

    update: controller example

--- apps/api/src/app/courses/courses.controller.ts
+++ apps/api/src/app/courses/courses.controller.ts
@@ -8,34 +8,67 @@ import {
   Put,
 } from '@nestjs/common';
 import { Course } from '../database/entities/course.entity';
-import { CoursesService } from './courses.service';
+
+let courses: Course[] = [
+  {
+    id: '6f311656-f8d6-407f-94c6-2bea0c4e5399',
+    title: 'Mock! Angular Fundamentals',
+    description: 'Pending',
+    thumbnail: '/assets/images/course-card-default.png',
+    lessons: [],
+  },
+  {
+    id: 'e358e4b1-7012-4353-9cf9-634e47ea787e',
+    title: 'Mock! RxJS Quickstart',
+    description: 'Pending.',
+    thumbnail: '/assets/images/course-card-default.png',
+    lessons: [],
+  },
+  {
+    id: '3dca6bc9-397f-4062-ae8d-319d36144b82',
+    title: 'Mock! Ionic Quickstart',
+    description: 'Pending.',
+    thumbnail: '/assets/images/course-card-default.png',
+    lessons: [],
+  },
+];
+
+const find = (id, collection) => collection.find((item) => item.id === id);
+
+const add = (entity, collection) => [...collection, entity];
+
+const update = (entity, collection) =>
+  collection.map((item) => (item.id === entity.id ? { ...entity } : item));
+
+const remove = (id, collection) => collection.filter((item) => item.id !== id);
 
 @Controller('courses')
 export class CoursesController {
-  constructor(private readonly coursesService: CoursesService) {}
-
   @Get()
-  all(): Promise<Course[]> {
-    return this.coursesService.getAll();
+  all(): Course[] {
+    return courses;
   }
 
   @Get(':id')
-  find(@Param() id: string): Promise<Course> {
-    return this.coursesService.get(id);
+  find(@Param() id: string): Course {
+    return find(id, courses);
   }
 
   @Post()
-  create(@Body() course: Course): Promise<Course> {
-    return this.coursesService.create(course);
+  create(@Body() course: Course): Course {
+    courses = add(course, courses);
+    return course;
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() course: Course): Promise<Course> {
-    return this.coursesService.update(course);
+  update(@Param('id') id: string, @Body() course: Course): Course {
+    courses = update(course, courses);
+    return course;
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return this.coursesService.delete(id);
+    courses = remove(id, courses);
+    return {};
   }
 }
