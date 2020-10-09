d837e7e95b89de29bbd85ad92acb60d389c0cf6d
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-10-09 08:55:03 -0700

    update: service example

--- apps/api/src/app/courses/courses.controller.ts
+++ apps/api/src/app/courses/courses.controller.ts
@@ -8,67 +8,34 @@ import {
   Put,
 } from '@nestjs/common';
 import { Course } from '../database/entities/course.entity';
-
-let courses: Course[] = [
-  {
-    id: '6f311656-f8d6-407f-94c6-2bea0c4e5399',
-    title: 'Mock! Angular Fundamentals',
-    description: 'Pending',
-    thumbnail: '/assets/images/course-card-default.png',
-    lessons: [],
-  },
-  {
-    id: 'e358e4b1-7012-4353-9cf9-634e47ea787e',
-    title: 'Mock! RxJS Quickstart',
-    description: 'Pending.',
-    thumbnail: '/assets/images/course-card-default.png',
-    lessons: [],
-  },
-  {
-    id: '3dca6bc9-397f-4062-ae8d-319d36144b82',
-    title: 'Mock! Ionic Quickstart',
-    description: 'Pending.',
-    thumbnail: '/assets/images/course-card-default.png',
-    lessons: [],
-  },
-];
-
-const find = (id, collection) => collection.find((item) => item.id === id);
-
-const add = (entity, collection) => [...collection, entity];
-
-const update = (entity, collection) =>
-  collection.map((item) => (item.id === entity.id ? { ...entity } : item));
-
-const remove = (id, collection) => collection.filter((item) => item.id !== id);
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private coursesService: CoursesService) {}
+
   @Get()
   all(): Course[] {
-    return courses;
+    return this.coursesService.all();
   }
 
   @Get(':id')
   find(@Param() id: string): Course {
-    return find(id, courses);
+    return this.coursesService.find(id);
   }
 
   @Post()
   create(@Body() course: Course): Course {
-    courses = add(course, courses);
-    return course;
+    return this.coursesService.create(course);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() course: Course): Course {
-    courses = update(course, courses);
-    return course;
+    return this.coursesService.update(course);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    courses = remove(id, courses);
-    return {};
+    return this.coursesService.delete(id);
   }
 }
--- apps/api/src/app/courses/courses.service.ts
+++ apps/api/src/app/courses/courses.service.ts
@@ -2,7 +2,63 @@ import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { DeleteResult, Repository } from 'typeorm';
 import { Course } from '../database/entities/course.entity';
 
+
+let courses: Course[] = [
+  {
+    id: '6f311656-f8d6-407f-94c6-2bea0c4e5399',
+    title: 'Mock! Angular Fundamentals',
+    description: 'Pending',
+    thumbnail: '/assets/images/course-card-default.png',
+    lessons: [],
+  },
+  {
+    id: 'e358e4b1-7012-4353-9cf9-634e47ea787e',
+    title: 'Mock! RxJS Quickstart',
+    description: 'Pending.',
+    thumbnail: '/assets/images/course-card-default.png',
+    lessons: [],
+  },
+  {
+    id: '3dca6bc9-397f-4062-ae8d-319d36144b82',
+    title: 'Mock! Ionic Quickstart',
+    description: 'Pending.',
+    thumbnail: '/assets/images/course-card-default.png',
+    lessons: [],
+  },
+];
+
+const find = (id, collection) => collection.find((item) => item.id === id);
+
+const add = (entity, collection) => [...collection, entity];
+
+const update = (entity, collection) =>
+  collection.map((item) => (item.id === entity.id ? { ...entity } : item));
+
+const remove = (id, collection) => collection.filter((item) => item.id !== id);
+
+
 @Injectable()
 export class CoursesService {
+  all(): Course[] {
+    return courses;
+  }
 
+  find(id: string): Course {
+    return find(id, courses);
+  }
+
+  create(course: Course): Course {
+    courses = add(course, courses);
+    return course;
+  }
+
+  update(course: Course): Course {
+    courses = update(course, courses);
+    return course;
+  }
+
+  delete(id: string) {
+    courses = remove(id, courses);
+    return {};
+  }
 }
